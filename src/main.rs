5f5d531aac92c350af822370948876de1964676c
Nekketsu <maherilaza11@gmail.com>
2024-09-23 12:46:17 +0300

    sum change

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,37 @@
 use core::arch::x86_64::_rdrand64_step;
 
 use colored::{ColoredString, Colorize};
+use std::{fs::OpenOptions, io::Write};
+
 const PASS : i8 = 1;
 
 fn main() -> () {
     
     let _check_arch = check_arch();
-
     let random_number: Result<u64, String> = gen_rand();
-    match random_number {
-        Ok(result) => {
-            println!("{} {}", "[*]".yellow() , result);
-        }
-        Err(e) => {
-            print!("{}", e);
-        }
+
+    if let Ok(nb_rand) = random_number {
+        println!("{} {}", "[*]".yellow() , nb_rand);
+
+        let nb_to_string = format!("{}\n", nb_rand);
+        let mut save_file = match OpenOptions::new()
+            .append(true)
+            .create(true)
+            .open("rand.txt") {
+                Ok(file) => file,
+                Err(e) => {
+                    println!("{e}");
+                    return;
+                }
+            };
+
+        let _write_file = match save_file.write_all(nb_to_string.as_bytes()) {
+            Ok(written_file) => written_file,
+            Err(e) => {
+                println!("{e}");
+                return;
+            }
+        };
     }
 
     return ();
