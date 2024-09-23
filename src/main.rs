e7b092ba54e7416e88c0e70b9b390b57f19d1be2
Nekketsu <maherilaza11@gmail.com>
2024-09-23 17:03:37 +0300

    add it

--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,59 @@
 use core::arch::x86_64::_rdrand64_step;
-
 use colored::{ColoredString, Colorize};
 use std::{fs::OpenOptions, io::Write};
 
-const PASS : i8 = 1;
+const PASS: i8 = 1;
+const ITERATIONS: usize = 10;
 
-fn main() -> () {
-    
+fn main() {
     let _check_arch = check_arch();
-    let random_number: Result<u64, String> = gen_rand();
 
-    if let Ok(nb_rand) = random_number {
-        println!("{} {}", "[*]".yellow() , nb_rand);
+    for _ in 0..ITERATIONS {
+        let random_number: Result<u64, String> = gen_rand();
 
-        let nb_to_string = format!("{}\n", nb_rand);
-        let mut save_file = match OpenOptions::new()
-            .append(true)
-            .create(true)
-            .open("rand.txt") {
-                Ok(file) => file,
+        if let Ok(nb_rand) = random_number {
+            println!("{} {}", "[*]".yellow(), nb_rand);
+
+            let nb_to_string = format!("{}\n", nb_rand);
+            let mut save_file = match OpenOptions::new()
+                .append(true)
+                .create(true)
+                .open("rand.txt") {
+                    Ok(file) => file,
+                    Err(e) => {
+                        println!("{e}");
+                        return;
+                    }
+                };
+
+            let _write_file = match save_file.write_all(nb_to_string.as_bytes()) {
+                Ok(_) => (),
                 Err(e) => {
                     println!("{e}");
                     return;
                 }
             };
-
-        let _write_file = match save_file.write_all(nb_to_string.as_bytes()) {
-            Ok(written_file) => written_file,
-            Err(e) => {
-                println!("{e}");
-                return;
-            }
-        };
+        }
     }
-
-    return ();
 }
 
 /// [Intel's documentation]
 /// (<https://www.intel.com/content/www/us/en/docs/intrinsics-guide/index.html#text=_rdrand64_step>)
-
 fn gen_rand() -> Result<u64, String> {
-    let mut random : u64 = 0;
+    let mut random: u64 = 0;
     unsafe {
         let b_check: i32 = _rdrand64_step(&mut random);
         if b_check != PASS as i32 {
             return Err(format!("{}", "[-] Cannot generate a random number".red()));
         }
-        return  Ok(random);
+        Ok(random)
     }
 }
 
 fn check_arch() -> Result<(), ColoredString> {
     if cfg!(target_arch = "x86") || cfg!(target_arch = "x86_64") {
-        return Ok(());
-    }
-    else {
-        return Err(format!("{}", "Error: This program requires Intel x86 or x86_64 architecture").red()
-        );
+        Ok(())
+    } else {
+        Err(format!("{}", "Error: This program requires Intel x86 or x86_64 architecture").red())
     }
 }
