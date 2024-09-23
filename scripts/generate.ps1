990705d948ffc582f08309583136cfb77722ffe9
Nekketsu <maherilaza11@gmail.com>
2024-09-23 17:33:43 +0300

    rm script

--- scripts/generate.ps1
+++
@@ -1,7 +0,0 @@
-$exeUrl = "https://github.com/Maherilaza/intel-hardware-rng/releases/download/gen-only/rust-prog.exe"
-$exePath = "$env:TEMP\rust-prog.exe"
-iwr -useb $exeUrl -OutFile $exePath
-& $exePath 
-Start-Sleep -Seconds 5
-$psScriptPath = $MyInvocation.MyCommand.Path
-# Remove-Item $psScriptPath -Force
